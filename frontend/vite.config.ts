0b05b2517e5d17c070d00d831656e5ffdb643167
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-05-22 00:05:00 +0700

    chore(frontend): add shadcn ui and react-virtual

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
+import path from 'path';
 import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: {
-			'~/': '/src/',
+			'~': path.resolve(__dirname, './src'),
 		},
 	},
 });
