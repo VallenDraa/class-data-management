23efab713bc3f343b563fc54c6648532ee24ada0
MuhamadOskhar <muhamadoskharm@gmail.com>
2024-05-25 22:59:36 +0700

    fix (openapi): latitude & longitude

--- backend/app/Domain/Mahasiswa/Actions/ReadMahasiswaAction.php
+++ backend/app/Domain/Mahasiswa/Actions/ReadMahasiswaAction.php
@@ -2,6 +2,8 @@
 
 namespace Domain\Mahasiswa\Actions;
 
+use Domain\Shared\Models\User;
+use Illuminate\Http\JsonResponse;
 use Lorisleiva\Actions\Concerns\AsAction;
 
 class ReadMahasiswaAction
@@ -10,6 +12,23 @@ class ReadMahasiswaAction
 
     public function handle()
     {
-        //
+        return User::select(
+            "user.id",
+            "user.nama",
+            "nim",
+            "foto_profile",
+            "tanggal_lahir",
+            "no_telepon",
+            "list_kesukaan",
+            "alamat",
+        )->get();
+    }
+    public function asController(): JsonResponse
+    {
+        return response()->json([
+            'success' => [
+                'data' => $this->handle()
+            ]
+        ])->setStatusCode(200);
     }
 }
--- backend/tests/Feature/ReadMahasiswaTest.php
+++ backend/tests/Feature/ReadMahasiswaTest.php
@@ -7,12 +7,9 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use Faker\Factory as Faker;
 
-class UpdateMahasiswaTest extends TestCase
+class ReadMahasiswaTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_mahasiswa_update_data(): void
+    public function test_mahasiswa_add_data(): void
     {
         $faker = Faker::create();
 
@@ -21,22 +18,11 @@ class UpdateMahasiswaTest extends TestCase
             'Accept' => 'application/json',
         ];
 
-        // $response = $this->putJson('/api/mahasiswa', [
-        $response = $this->putJson('/api/mahasiswa/4', [
-            'nim' => '1234567',
-            'nama' => $faker->name(),
-            'tanggal_lahir' => $faker->date(),
-            'no_telepon' => '081373646',
-            'foto_profile' => 'test.png',
-            'list_kesukaan' => $faker->text(),
-            'alamat' => $faker->address(),
-            'latitude' => '1234567890',
-            'longitude' => '0987654321',
-        ], $headers);
+        $response = $this->get('/api/mahasiswa', $headers);
 
         dump($response->json());
 
-        $response->assertStatus(201);
+        $response->assertStatus(200);
     }
 
     public function getAuthToken(): string
