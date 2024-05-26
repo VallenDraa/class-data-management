45a980ad958d6bd205b5c99ef94f7f5aae608cfe
MuhamadOskhar <muhamadoskharm@gmail.com>
2024-05-26 14:00:33 +0700

    perf (laravel): meningkatkan keterbacaan policy

---
+++ backend/app/Domain/Mahasiswa/MahasiswaPolicyTrait.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Domain\Mahasiswa;
+
+use Domain\Shared\Enums\UserRoleses;
+
+trait MahasiswaPolicyTrait
+{
+    public function canAddMahasiswa(): bool
+    {
+        return match ($this) {
+            UserRoleses::Admin => true,
+            UserRoleses::Mahasiswa => false,
+        };
+    }
+
+    public function canUpdateOtherMahasiswa(): bool
+    {
+        return match ($this) {
+            UserRoleses::Admin => true,
+            UserRoleses::Mahasiswa => false,
+        };
+    }
+
+    public function canDeleteMahasiswa(): bool
+    {
+        return match ($this) {
+            UserRoleses::Admin => true,
+            UserRoleses::Mahasiswa => false,
+        };
+    }
+
+    public function canReadHistory(): bool
+    {
+        return match ($this) {
+            UserRoleses::Admin => true,
+            UserRoleses::Mahasiswa => false,
+        };
+    }
+}
