051ef2bd59a1a3d8f452339305bbddb5b6474bab
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-06-05 00:30:35 +0700

    chore: add vite-plugin-pwa and workbox-window dependencies, update theme color and icons

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,50 @@
 import path from 'path';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 
+const manifestForPlugin: Partial<VitePWAOptions> = {
+	registerType: 'prompt',
+	includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+	manifest: {
+		name: 'Kelass',
+		short_name: 'Kelass',
+		description: 'Aplikasi manajemen mahasiswa terbaik senusantara.',
+		icons: [
+			{
+				src: '/android-chrome-192x192.png',
+				sizes: '192x192',
+				type: 'image/png',
+			},
+			{
+				src: '/android-chrome-512x512.png',
+				sizes: '512x512',
+				type: 'image/png',
+			},
+			{
+				src: '/apple-touch-icon.png',
+				sizes: '180x180',
+				type: 'image/png',
+				purpose: 'apple touch icon',
+			},
+			{
+				src: '/maskable_icon.png',
+				sizes: '225x225',
+				type: 'image/png',
+				purpose: 'any maskable',
+			},
+		],
+		theme_color: '#0284c7',
+		background_color: '#ffffff',
+		display: 'standalone',
+		scope: '/',
+		start_url: '/',
+		orientation: 'portrait',
+	},
+};
+
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), VitePWA(manifestForPlugin)],
 	resolve: {
 		alias: {
 			'~': path.resolve(__dirname, './src'),
