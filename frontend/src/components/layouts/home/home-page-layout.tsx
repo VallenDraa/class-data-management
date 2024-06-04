59049eb2513e0adbdc153a9c34ba2dcf369652a0
VallenDraa <jestinevallendradwiputra@gmail.com>
2024-06-04 23:42:43 +0700

    feat: update site footer

--- frontend/src/components/layouts/home/home-page-layout.tsx
+++ frontend/src/components/layouts/home/home-page-layout.tsx
@@ -15,7 +15,7 @@ export function HomePageLayout(props: PageLayoutProps) {
 			{children}
 
 			<div className="flex justify-between my-1.5 text-[0.65rem] text-neutral-500">
-				<p>Dibuat oleh kelompok 4</p>
+				<p>Made By Kelompok 4 With 💖</p>
 
 				<p>2024 | IMK</p>
 			</div>
